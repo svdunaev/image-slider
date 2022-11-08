81ff7e654a2e5d910a8e56af5e5479b448da8521
svdunaev <54572104+svdunaev@users.noreply.github.com>
2022-11-08 21:22:59 +0500

    feat: add slider functionality

---
+++ app.js
@@ -0,0 +1,46 @@
+const upBtn = document.querySelector('.up-button');
+const downBtn = document.querySelector('.down-button');
+
+const sideBar = document.querySelector('.sidebar');
+
+const mainSlide = document.querySelector('.main-slide');
+
+const container = document.querySelector('.container');
+
+const slidesCount = mainSlide.querySelectorAll('div').length;
+let activeSlideIndex = 0;
+
+sideBar.style.top = `-${(slidesCount - 1) * 100}vh`;
+
+upBtn.addEventListener('click', () => {
+	changeSlide('up')
+});
+downBtn.addEventListener('click', () => {
+	changeSlide('down')
+});
+
+function changeSlide(direction) {
+	const height = container.clientHeight;
+
+	switch (direction) {
+		case 'up':
+			activeSlideIndex++
+			if ( activeSlideIndex === slidesCount) {
+				activeSlideIndex = 0;
+			}
+			break;
+
+		case 'down':
+			activeSlideIndex--
+			if(activeSlideIndex < 0) {
+				activeSlideIndex = slidesCount - 1;
+			}
+			break;
+
+		default:
+			break;
+	}
+
+	mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
+	sideBar.style.transform = `translateY(${activeSlideIndex * height}px)`
+}
